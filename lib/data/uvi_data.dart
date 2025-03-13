a25ffa664bc9bae160703bc3913c9e8978481db1
Garvin Tang <garvin9006@gmail.com>
2025-03-13 20:30:23 +1100

    changed uvi api to openuv.io

--- lib/data/uvi_data.dart
+++ lib/data/uvi_data.dart
@@ -1,38 +1,17 @@
-
-class UVITime {
-  final String time;
-  final double uvi;
-
-  UVITime({required this.time, required this.uvi});
-
-  factory UVITime.fromJson(Map<String, dynamic> json) {
-    return UVITime(
-      time: json['time'] as String,
-      uvi: json['uvi'] as double,
-    );
-  }
-}
-
 class UVIData {
-  final double latitude;
-  final double longitude;
-  final UVITime now;
-  final List<UVITime> forecast;
-  final List<UVITime> history;
+  final double uv;
+  final String uvTime;
 
-  UVIData({required this.latitude, required this.longitude, required this.now, required this.forecast, required this.history});
+  UVIData({required this.uv, required this.uvTime});
 
   factory UVIData.fromJson(Map<String, dynamic> json) {
+    var res = json['result'];
+    if (res == null) {
+      throw Exception('Failed to load UVI data');
+    }
     return UVIData(
-      latitude: json['latitude'] as double,
-      longitude: json['longitude'] as double,
-      now: UVITime.fromJson(json['now']),
-      forecast: (json['forecast'] as List)
-          .map((item) => UVITime.fromJson(item))
-          .toList(),
-      history: (json['history'] as List)
-          .map((item) => UVITime.fromJson(item))
-          .toList(),
+      uv: res['uv'],
+      uvTime: res['uv_time'],
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,11 +10,26 @@ import 'package:onboarding/data/uvi_data.dart';
 import 'package:onboarding/widgets/location_search_bar.dart';
 
 Future<UVIData> fetchUVI(Map<String, dynamic> place) async{
-  final response = await http.get(Uri.parse('https://currentuvindex.com/api/v1/uvi?latitude=${place['lat']}&longitude=${place['lon']}'));
+  // final response = await http.get(Uri.parse('https://currentuvindex.com/api/v1/uvi?latitude=${place['lat']}&longitude=${place['lon']}'));
 
-  if (response.statusCode == 200) {
-    return UVIData.fromJson(jsonDecode(response.body));
-  } else {
+  final String apiUrl = "https://api.openuv.io/api/v1/uv?lat=-37.93&lng=145.13&alt=100&dt=";
+  final String accessToken = "openuv-c10rrm87499zf-io";
+
+  try {
+    final response = await http.get(
+      Uri.parse(apiUrl),
+      headers: {
+        "x-access-token": accessToken,
+        "Content-Type": "application/json",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return UVIData.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load UVI data');
+    }
+  } catch (e) {
     throw Exception('Failed to load UVI data');
   }
 }
@@ -65,9 +80,9 @@ class _HomePageState extends State<HomePage> {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Text('UV ${snapshot.data!.now.uvi.toInt().toString()}'),
-                      Text(DateFormat('hh:mm a').format(DateTime.parse(snapshot.data!.now.time.toString()).toLocal())),
-                      Text('Retrieved from https://currentuvindex.com'),
+                      Text('UV ${snapshot.data!.uv.toInt().toString()}'),
+                      Text('Last Updated: ${DateFormat('E dd/MM, hh:mm a').format(DateTime.parse(snapshot.data!.uvTime.toString()).toLocal())}'),
+                      Text('Retrieved from https://www.openuv.io/'),
                     ]
                   );
                 } else if (snapshot.hasError) {
