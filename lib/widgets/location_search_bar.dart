5c70cc46959d3613e94ac0d80f76033105b92d1f
Garvin Tang <garvin9006@gmail.com>
2025-03-13 16:14:21 +1100

    created location search bar that suggests top 5 locations in Australia based on user's input

---
+++ lib/widgets/location_search_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class LocationSearchBar extends StatefulWidget {
+
+  final Function(Map<String, dynamic>) onValueChanged;
+
+  const LocationSearchBar({super.key, required this.onValueChanged});
+
+  @override
+  _LocationSearchBarState createState() => _LocationSearchBarState();
+}
+
+class _LocationSearchBarState extends State<LocationSearchBar> {
+  final TextEditingController _controller = TextEditingController();
+
+  Future<List<Map<String, dynamic>>> _searchLocation(String query) async {
+    if (query.isEmpty) return [];
+
+    // retrieve top 5 locations from OpenStreetMap in Australia based on user input
+    final url = Uri.parse(
+        "https://nominatim.openstreetmap.org/search?q=$query&format=json&addressdetails=1&limit=5&countrycodes=AU");
+    
+    final response = await http.get(url);
+    
+    if (response.statusCode == 200) {
+      List data = json.decode(response.body);
+      return data.map((place) {
+        return {
+          "name": place["display_name"],
+          "lat": double.parse(place["lat"]),
+          "lon": double.parse(place["lon"]),
+        };
+      }).toList();
+    } else {
+      throw Exception("Failed to fetch locations");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TypeAheadField<Map<String, dynamic>>(
+      builder: (context, controller, focusNode) {
+        return TextField(
+          controller: controller,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+            labelText: "Search Location",
+            border: OutlineInputBorder(),
+            prefixIcon: Icon(Icons.location_on),
+          ),
+        );},
+      suggestionsCallback: (query) => _searchLocation(query),
+      itemBuilder: (context, suggestion) {
+        return ListTile(
+          leading: Icon(Icons.place),
+          title: Text(suggestion["name"]),  // location suggestions to be displayed in the text field
+        );
+      },
+      onSelected: (suggestion) {
+        _controller.text = suggestion["name"];  // update text field with selected location
+        widget.onValueChanged({'lat': suggestion["lat"], 'lon': suggestion["lon"]});  // update coordinates for uvi
+      },
+    );
+  }
+}
