2e25d09b0e1beda73922192f5489edc325ac81b6
yyan0391 <yyan0391@student.monash.edu>
2025-03-15 19:06:38 +1100

    change sun icon to lottie Animation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,9 +6,14 @@ import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:onboarding/data/uvi_data.dart';
 import 'package:onboarding/widgets/location_search_bar.dart';
+import 'package:lottie/lottie.dart';
 
-Future<UVIData> fetchUVI(Map<String, dynamic> place, Function(String) updateSource) async{
-  final String apiUrl = "https://api.openuv.io/api/v1/uv?lat=${place['lat']}&lng=${place['long']}&alt=100&dt=";
+Future<UVIData> fetchUVI(
+  Map<String, dynamic> place,
+  Function(String) updateSource,
+) async {
+  final String apiUrl =
+      "https://api.openuv.io/api/v1/uv?lat=${place['lat']}&lng=${place['long']}&alt=100&dt=";
   final String accessToken = dotenv.get('OPEN_UV_API_KEY');
 
   try {
@@ -24,22 +29,26 @@ Future<UVIData> fetchUVI(Map<String, dynamic> place, Function(String) updateSour
       updateSource('https://www.openuv.io/');
       return UVIData.fromJson(jsonDecode(response.body));
     } else {
-        throw Exception('Failed to load UVI data');
+      throw Exception('Failed to load UVI data');
     }
   } catch (e) {
     // attempt to get UVI data from another source
-      try {
-          final response = await http.get(Uri.parse('https://currentuvindex.com/api/v1/uvi?latitude=${place['lat']}&longitude=${place['lon']}'));
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://currentuvindex.com/api/v1/uvi?latitude=${place['lat']}&longitude=${place['lon']}',
+        ),
+      );
 
-          if (response.statusCode == 200) {
-            updateSource('https://currentuvindex.com');
-            return UVIData.fromCurrentUVAPIJson(jsonDecode(response.body));
-          } else {
-            throw Exception('Failed to load UVI data');
-          }
-      } catch (e) {
+      if (response.statusCode == 200) {
+        updateSource('https://currentuvindex.com');
+        return UVIData.fromCurrentUVAPIJson(jsonDecode(response.body));
+      } else {
         throw Exception('Failed to load UVI data');
       }
+    } catch (e) {
+      throw Exception('Failed to load UVI data');
+    }
   }
 }
 
@@ -53,12 +62,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Map<String, dynamic> _selectedPlace = {"lat":0.0, "lon":0.0};   //TODO: change to current location
-  Map<String, dynamic> _selectedPlace = {"lat":-37.8142, "lon":144.9631}; 
+  Map<String, dynamic> _selectedPlace = {"lat": -37.8142, "lon": 144.9631};
   late Future<UVIData> _futureUVIData;
   String _uvAPISource = '';
-  Color _sunColor = Colors.yellow;
+  // Color _sunColor = Colors.yellow;
 
-  String _selectedPlaceName = "Melbourne, Australia"; 
+  String _selectedPlaceName = "Melbourne, Australia";
 
   void _updatePlace(Map<String, dynamic> place) {
     setState(() {
@@ -78,24 +87,30 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     //TODO: get current coordinates
-    _futureUVIData = fetchUVI({'lat': -37.8142454, 'lon': 144.9631732}, _updateSource);
+    _futureUVIData = fetchUVI({
+      'lat': -37.8142454,
+      'lon': 144.9631732,
+    }, _updateSource);
   }
 
-  Widget _buildLocation(){
+  Widget _buildLocation() {
     return Card(
-  margin: EdgeInsets.all(16),
-  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-  elevation: 4,
-  child: ListTile(
-    leading: Icon(Icons.location_on, color: Colors.redAccent),
-    title: Text(_selectedPlaceName,
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-    // subtitle: Text("Current Location"),
-    subtitle: Text('Lat: ${_selectedPlace["lat"]}, Lon: ${_selectedPlace["lon"]}'),
-  ),
-);
+      margin: EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      elevation: 4,
+      child: ListTile(
+        leading: Icon(Icons.location_on, color: Colors.redAccent),
+        title: Text(
+          _selectedPlaceName,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        // subtitle: Text("Current Location"),
+        subtitle: Text(
+          'Lat: ${_selectedPlace["lat"]}, Lon: ${_selectedPlace["lon"]}',
+        ),
+      ),
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -105,22 +120,50 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(children: <Widget>[
-          LocationSearchBar(onValueChanged: _updatePlace),
-          _buildLocation(),
-          // Text('${_selectedPlace["lat"]} ${_selectedPlace["lon"]}'),
-          FutureBuilder<UVIData>(
+        child: Column(
+          children: <Widget>[
+            LocationSearchBar(onValueChanged: _updatePlace),
+            _buildLocation(),
+            // Text('${_selectedPlace["lat"]} ${_selectedPlace["lon"]}'),
+            FutureBuilder<UVIData>(
               future: _futureUVIData,
               builder: (context, snapshot) {
+                double lottieWidth = 150;
+                double lottieHeight = 150;
+
                 if (snapshot.hasData) {
+                  double uvIndex = snapshot.data!.uv;
+                  // double uvIndex = 8;
+                  // UV with different Animation
+                  String lottieAsset;
+                  if (uvIndex <= 2) {
+                    lottieAsset = "assets/lottie/cloudy.json"; // UV low
+                  } else if (uvIndex <= 5) {
+                    lottieAsset = "assets/lottie/sun.json"; // UV Moderate
+                    lottieWidth = 250;
+                    lottieHeight = 250;
+                  } else {
+                    lottieAsset = "assets/lottie/warning.json"; // UV high
+                  }
+
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Icon(Icons.wb_sunny, color: _sunColor, size: 100),
+                      // Icon(Icons.wb_sunny, color: _sunColor, size: 100),
+                      Lottie.asset(
+                        lottieAsset,
+                        width: lottieWidth,
+                        height: lottieHeight,
+                        repeat: true,
+                        animate: true,
+                      ),
+
                       Text('UV ${snapshot.data!.uv.toInt().toString()}'),
-                      Text('Last Updated: ${DateFormat('E dd/MM, hh:mm a').format(DateTime.parse(snapshot.data!.uvTime.toString()).toLocal())}'),
+                      Text(
+                        'Last Updated: ${DateFormat('E dd/MM, hh:mm a').format(DateTime.parse(snapshot.data!.uvTime.toString()).toLocal())}',
+                      ),
                       Text('Retrieved from $_uvAPISource'),
-                    ]
+                    ],
                   );
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
@@ -129,8 +172,9 @@ class _HomePageState extends State<HomePage> {
                 }
               },
             ),
-        ])
+          ],
         ),
-      );
+      ),
+    );
   }
 }
